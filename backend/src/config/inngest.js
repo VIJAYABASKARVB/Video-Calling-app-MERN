84337946893857ae4e34c186b1480a9e4468b3b0
Vijaya Baskar P <vbluben@gmail.com>
2026-04-01 20:57:41 +0530

    "fix: safe access on emailAddresses"

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,10 +1,3 @@
-import { Inngest } from "inngest";
-import {connectDB} from "./db.js"
-import {User} from "../models/user.model.js"
-
-// Create a client to send and receive events
-export const inngest = new Inngest({ id: "video-calling-app" });
-
 /*
 
 The flow for the below function:
@@ -25,27 +18,31 @@ User saved in MongoDB
 
 */
 
+import { Inngest } from "inngest";
+import {connectDB} from "./db.js"
+import {User} from "../models/user.model.js"
+
+export const inngest = new Inngest({ id: "video-calling-app" });
+
 const syncUser = inngest.createFunction(
   {id:"sync user"},
   {event:"clerk/user.created"},
   async ({event}) => {
     await connectDB()
 
-    const {id,email_address,first_name,last_name,image_url} = event.data;
+    const {id, email_addresses, first_name, last_name, image_url} = event.data;
 
-  const newUser = {
-    clerkId: id,
-    email: email_addresses?.[0]?.email_address,
-    name: `${first_name || ""} ${last_name || ""}`.trim(),
-    image: image_url,
-  }
+    const newUser = {
+      clerkId: id,
+      email: email_addresses?.[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
+      image: image_url,
+    }
 
     await User.create(newUser);
   }
 );
 
-
-//Deleting the user from the DB
 const deleteUserFromDB = inngest.createFunction(
   {id:"delete-user-from-database"},
   {event:"clerk/user.deleted"},
@@ -53,9 +50,7 @@ const deleteUserFromDB = inngest.createFunction(
     await connectDB();
     const {id} = event.data;
     await User.deleteOne({clerkId:id});
-
   }
 )
 
-// Create an empty array where we'll export future Inngest functions
-export const functions = [syncUser,deleteUserFromDB];
+export const functions = [syncUser, deleteUserFromDB];
